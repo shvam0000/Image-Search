ddeb86672935c3ae4d4721c034a86388e9ea46b2
Shivam Shekhar <shvam0000@gmail.com>
2021-07-22 23:48:22 +0530

    💄 UI Tweaks

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import Download from './UI/Icons/Download';
 import Like from './UI/Icons/Like';
+import Redirect from './UI/Icons/Redirect';
+
 const Card = ({ image }) => {
   const [isLiked, setIsLiked] = useState(false);
   const tags = image.tags.split(',');
@@ -9,15 +11,20 @@ const Card = ({ image }) => {
     setIsLiked(!isLiked);
   };
 
+  const redirectHandler = () => {
+    let path = image.pageURL;
+    window.location.href = path;
+  };
+
   return (
-    <div class='flex flex-col mt-3 items-center justify-center max-w-sm mx-auto'>
+    <div className='flex flex-col mt-3 items-center justify-center max-w-sm mx-auto font-montserrat'>
       <img
-        className='w-auto h-auto bg-gray-300 bg-center bg-cover rounded-lg shadow-md'
+        className='w-auto h-auto rounded-lg shadow-2xl'
         src={image.webformatURL}
         alt='Loading...'
       />
 
-      <div className='w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800'>
+      <div className='w-56 -mt-10 bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800'>
         <h3 className='py-2 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white'>
           {tags.map((tag, index) => (
             <span
@@ -32,14 +39,16 @@ const Card = ({ image }) => {
           <button
             className={
               isLiked
-                ? 'text-red-500 focus:outline-none'
-                : 'text-black focus:outline-none'
+                ? 'text-red-500 focus:outline-none text-2xl'
+                : 'text-black focus:outline-none text-2xl'
             }
             onClick={likeHandler}>
             <Like />
           </button>
-          <button className='px-2 py-1 text-xl font-semibold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none'>
-            <Download />
+          <button
+            onClick={redirectHandler}
+            className='px-2 py-1 text-xl font-semibold text-white uppercase transition-colors duration-200 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none'>
+            <Redirect />
           </button>
         </div>
       </div>
--- src/components/Search.js
+++ src/components/Search.js
@@ -8,23 +8,28 @@ const ImageSearch = ({ searchText }) => {
     searchText(text);
   };
 
+  const downloadImage = (e) => {
+    console.log('hey');
+  };
+
   return (
-    <div className='flex justify-center my-10'>
+    <div className='flex justify-center my-10 font-montserrat'>
       <form
         onSubmit={onSubmitHandler}
         className='w-full max-w-sm flex flex-col'>
         <div>
           <input
-            className='border-b-4 border-gray-500 w-2/3 h-11 bg-gray-100 focus:outline-none focus:border-gray-700'
+            className='border-b-4 border-gray-500 w-2/3 h-11 text-center bg-gray-100 focus:outline-none focus:border-gray-700'
             onChange={(e) => {
               setText(e.target.value);
             }}
             type='text'
-            placeholder='Search Image Term...'
+            placeholder='Search...'
           />
           <button
             className='bg-blue-700 text-white text-xl px-5 py-2 rounded-lg ml-5 hover:bg-blue-600 focus:outline-none focus:bg-blue-700'
-            type='submit'>
+            type='submit'
+            onClick={downloadImage}>
             Search
           </button>
         </div>
